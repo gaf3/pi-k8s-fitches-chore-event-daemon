c98833abc92ef047686561068d209f0cb673308b
Gaffer Fitch <gaffer.fitch@gmail.com>
2018-11-26 05:56:49 +0000

    Initial create

---
+++ lib/service.py
@@ -0,0 +1,75 @@
+"""
+Main module for daemon
+"""
+
+import os
+import time
+import json
+import traceback
+
+import redis
+import pi_k8s_fitches.chore_redis
+
+
+class Daemon(object):
+    """
+    Main class for daemon
+    """
+
+    def __init__(self):
+
+        self.chore_redis = pi_k8s_fitches.chore_redis.ChoreRedis(
+            host=os.environ['REDIS_HOST'],
+            port=int(os.environ['REDIS_PORT']),
+            channel=os.environ['REDIS_SPEECH_CHANNEL']
+        )
+        self.redis = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=int(os.environ['REDIS_PORT']))
+        self.channel = os.environ['REDIS_EVENT_CHANNEL']
+        self.sleep = int(os.environ['SLEEP'])
+        self.pubsub = None
+
+    def subscribe(self):
+        """
+        Subscribes to the event channel on Redis
+        """
+
+        self.pubsub = self.redis.pubsub()
+        self.pubsub.subscribe(self.channel) 
+
+    def process(self, start):
+        """
+        Processes a message from the channel if later than the daemons start time
+        """
+
+        message = self.pubsub.get_message()
+
+        if not message:
+            return
+
+        data = json.loads(message['data'])
+
+        if data["timestamp"] < start or data["type"] != "rising" or data["gpio_port"] != 4:
+            return
+
+        chore = self.chore_redis.get(data["node"])
+
+        if not chore:
+            return
+
+        self.chore_redis.next(chore)
+            
+    def run(self):
+        """
+        Runs the daemon
+        """
+
+        start = time.time()
+        self.subscribe()
+
+        while True:
+            try:
+                self.process(start)
+                time.sleep(self.sleep)
+            except Exception as exception:
+                print(str(exception))
+                print(traceback.format_exc())
---
+++ test/test_service.py
@@ -0,0 +1,190 @@
+import unittest
+import unittest.mock
+
+import os
+import json
+
+import service
+
+class MockChoreRedis(object):
+
+    def __init__(self, host, port, channel):
+
+        self.host = host
+        self.port = port
+        self.channel = channel
+
+        self.chores = {}
+        self.nexted = []
+
+    def get(self, node):
+
+        if node in self.chores:
+            return self.chores[node]
+
+        return None
+
+    def next(self, chore):
+
+        self.nexted.append(chore)
+
+class MockRedis(object):
+
+    def __init__(self, host, port):
+
+        self.host = host
+        self.port = port
+        self.channel = None
+        self.messages = []
+
+    def pubsub(self):
+
+        return self
+
+    def subscribe(self, channel):
+
+        self.channel = channel
+
+    def get_message(self):
+
+        return self.messages.pop(0)
+
+class TestService(unittest.TestCase):
+
+    @unittest.mock.patch.dict(os.environ, {
+        "REDIS_HOST": "data.com",
+        "REDIS_PORT": "667",
+        "REDIS_SPEECH_CHANNEL": "stuff",
+        "REDIS_EVENT_CHANNEL": "things",
+        "SLEEP": "7"
+    })
+    @unittest.mock.patch("pi_k8s_fitches.chore_redis.ChoreRedis", MockChoreRedis)
+    @unittest.mock.patch("redis.StrictRedis", MockRedis)
+    def setUp(self):
+
+        self.daemon = service.Daemon()
+
+    def test___init___(self):
+
+        self.assertEqual(self.daemon.chore_redis.host, "data.com")
+        self.assertEqual(self.daemon.chore_redis.port, 667)
+        self.assertEqual(self.daemon.chore_redis.channel, "stuff")
+        self.assertEqual(self.daemon.redis.host, "data.com")
+        self.assertEqual(self.daemon.redis.port, 667)
+        self.assertEqual(self.daemon.channel, "things")
+        self.assertEqual(self.daemon.sleep, 7)
+        self.assertIsNone(self.daemon.pubsub)
+
+    def test_subscribe(self):
+
+        self.daemon.subscribe()
+
+        self.assertEqual(self.daemon.redis, self.daemon.pubsub)
+        self.assertEqual(self.daemon.redis.channel, "things")
+
+    def test_process(self):
+
+        self.daemon.subscribe()
+
+        self.daemon.redis.messages = [
+            None,
+            {
+                "data": json.dumps({
+                    "timestamp": 0
+                })
+            },
+            {
+                "data": json.dumps({
+                    "timestamp": 7,
+                    "type": "falling"
+                })
+            },
+            {
+                "data": json.dumps({
+                    "timestamp": 7,
+                    "type": "rising",
+                    "gpio_port": 0
+                })
+            },
+            {
+                "data": json.dumps({
+                    "timestamp": 7,
+                    "type": "rising",
+                    "gpio_port": 4,
+                    "node": "nope"
+                })
+            },
+            {
+                "data": json.dumps({
+                    "timestamp": 7,
+                    "type": "rising",
+                    "gpio_port": 4,
+                    "node": "bump"
+                })
+            }
+        ]
+        self.daemon.chore_redis.chores = {
+            "bump": {
+                "text": "do it"
+            }
+        }
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [])
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [])
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [])
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [])
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [])
+
+        self.daemon.process(7)
+        self.assertEqual(self.daemon.chore_redis.nexted, [{
+            "text": "do it"
+        }])
+
+    @unittest.mock.patch("service.time.time")
+    @unittest.mock.patch("service.time.sleep")
+    @unittest.mock.patch("traceback.format_exc")
+    @unittest.mock.patch('builtins.print')
+    def test_run(self, mock_print, mock_traceback, mock_sleep, mock_time):
+
+        mock_time.return_value = 7
+
+        self.daemon.redis.messages = [
+            {
+                "data": json.dumps({
+                    "timestamp": 7,
+                    "type": "rising",
+                    "gpio_port": 4,
+                    "node": "bump"
+                })
+            },
+            None,
+            None
+        ]
+        self.daemon.chore_redis.chores = {
+            "bump": {
+                "text": "do it"
+            }
+        }
+
+        mock_sleep.side_effect = [None, Exception("whoops"), Exception("whoops")]
+        mock_traceback.side_effect = ["spirograph", Exception("doh")]
+
+        self.assertRaisesRegex(Exception, "doh", self.daemon.run)
+
+        self.assertEqual(self.daemon.chore_redis.nexted, [{
+            "text": "do it"
+        }])
+        mock_print.assert_has_calls([
+            unittest.mock.call("whoops"),
+            unittest.mock.call("spirograph"),
+            unittest.mock.call("whoops")
+        ])
